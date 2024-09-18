cfa9c1ce20d64e0a0a7229c13e3e11f6d0be9480
Chij114 <chij114@proton.me>
2024-09-18 16:48:58 -0400

    Completed Assignment (1)

--- Assignment1.java
+++ Assignment1.java
@@ -1,7 +1,63 @@
 // Import libraries here
+import java.util.*;
 
 public class Assignment1 {
   public static void main(String[] args) {
     // Add your code here
+
+    float userTemp, celsiusTemp;
+    String randomString, newRandString, finalString;
+    char char1, char2, char3;
+    Random randomNumGen;
+    int randomNum;
+
+    Scanner scan = new Scanner(System.in);
+
+    //Initials
+    System.out.println();
+    System.out.println("  CCCCCC\tEEEEEEEE");
+    System.out.println(" CCCCCCC\tEEEEEEE");
+    System.out.println("CCC\t\tEEE");
+    System.out.println("CCC\t\tEEE");
+    System.out.println("CCC\t\tEEEEEEE");
+    System.out.println("CCC\t\tEEEEEEE");
+    System.out.println("CCC\t\tEEE");
+    System.out.println("CCC\t\tEEE");
+    System.out.println(" CCCCCCC\tEEEEEEE");
+    System.out.println("  CCCCCC\tEEEEEEEE");
+    System.out.println();
+
+    //Temperature
+    System.out.println("Please enter a temperature in Fahrenheit below:");
+
+    userTemp = scan.nextFloat();
+
+    celsiusTemp = (userTemp - 32) * 5 / 9;
+
+    //Random String
+    System.out.println("Please enter a random sequence of five letters below:");
+    
+    randomString = scan.next();
+ 
+    scan.close();
+
+    //New String
+    char1 = randomString.charAt(1);
+    char2 = randomString.charAt(2);
+    char3 = randomString.charAt(3);
+  
+    newRandString = "" + char3 + char2 + char1;
+
+    //Random Number Generator
+    System.out.println("Generating random number...");
+
+    randomNumGen = new Random();
+
+    randomNum = randomNumGen.nextInt(16353) + 32;
+
+    //Final Combination
+    finalString = "" + celsiusTemp + newRandString + randomNum;
+
+    System.out.println("Your final string is: " + finalString);
   }
 }
